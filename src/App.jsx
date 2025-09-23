549905c6648b80bc43afd31607538dbf2187f977
Srijan44-44 <145187583+Srijan44-44@users.noreply.github.com>
2025-09-23 14:49:43 +0530

    Resolve merge conflict in src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import React from 'react';
-import HospitalDashboard from './HospitalDashboard';
-
-function App() {
-  return <HospitalDashboard />;
-=======
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PatientDashboard from "./PatientDashboard";
-import BookConsultationApp from "./book_consultationmain/App"; // 👈 booking flow
+import BookConsultationApp from "./book_consultationmain/App"; // booking flow
 
 function App() {
   return (
@@ -22,7 +15,6 @@ function App() {
       </Routes>
     </Router>
   );
->>>>>>> 98b4bdd2f0f1adda8cc66137f35caeaab430e2df
 }
 
 export default App;
